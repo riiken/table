bb3d6bd41d093ab90731192b8fb8ccf64e4f2ff3
riiken <rai22160@gmail.com>
2021-04-29 14:27:56 -0400

    open

---
+++ src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'table';
+ulist=Array();
+data=Array();
+user:any;
+isDesc:any;
+way:any;
+// 
+
+ adduser(naam,saal){
+   if(naam.value.length>0 &&saal.value.length>0){
+   this.ulist.push({
+     name:naam.value,
+     age:saal.value
+   });
+   naam.value="";
+   saal.value="";
+  }
+
+ }
+
+ delete(item){
+   this.ulist.splice(item,1);
+ }
+
+
+ sortData(){
+   if(this.user){
+   let newarr=this.ulist.sort((a,b)=>a.age - b.age);
+   this.data=newarr;
+ }else{
+  let newarr=this.ulist.sort((a,b)=>b.age - a.age);
+  this.data=newarr;
+ }
+ this.user=!this.user;
+}
+
+sortName(property){
+  this.isDesc=!this.isDesc;
+  let way=this.isDesc?1:-1;
+  this.ulist.sort(function (a,b) {
+    if(a[property]<b[property]){
+      return -1*way;;
+    }else if(a[property]>b[property]){
+      return 1*way;
+    }
+    else{
+      return 0;
+    }
+  })
+}
+}
+
